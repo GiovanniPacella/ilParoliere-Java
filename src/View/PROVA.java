edd88518976e002389224a578d96dfec40363e63
PELANGELO <riccardo.angelo@edu.cobianchi.it>
2023-02-14 11:30:16 +0100

    COMMITPROVA Form e .Java

--- src/View/PROVA.java
+++ src/View/PROVA.java
@@ -7,10 +7,11 @@ import java.awt.event.ComponentAdapter;
 public class PROVA extends JFrame{
     private JRadioButton RadioButton;
     private JPanel PanelMain;
+    private JButton button1;
 
     public PROVA() {
 
 
-
+//dsfdsfsdfsdfsd
     }
 }
