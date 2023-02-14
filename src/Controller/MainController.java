5301067fe0a3d9e86f6bd5fe1b56a413fffd2f39
PELANGELO <riccardo.angelo@edu.cobianchi.it>
2023-02-14 11:22:14 +0100

    COMMprova

--- src/Controller/MainController.java
+++ src/Controller/MainController.java
@@ -1,8 +1,13 @@
 package Controller;
 
+import View.PROVA;
+
 public class MainController {
     public static void main(String[] args) {
         System.out.println("CIAO" + "12345");
+
+        PROVA p=new PROVA();
+
 
     }
 }
