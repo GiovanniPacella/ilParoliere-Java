be84ef023959ac759bf0d3f5098f5449ac60e090
PELANGELO <riccardo.angelo@edu.cobianchi.it>
2023-02-14 11:46:53 +0100

    COMMIT Main appo'

--- src/Controller/MainController.java
+++ src/Controller/MainController.java
@@ -5,27 +5,13 @@ import View.PROVA;
 import javax.swing.*;
 import java.lang.Math;
 public class MainController {
-    private int nRigheColonne=5;
-    private char lettere[]= {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-    private char Board[][] = new char[nRigheColonne][nRigheColonne];
-
-    public void creaBoard()
-    {
-        for(int i=0; i<nRigheColonne; i++)
-        {
-            for(int j=0; j<nRigheColonne; j++)
-            {
-                int nCasuale= (int) (Math.random()*lettere.length);
-                Board[i][j]=lettere[nCasuale];
-            }
-        }
-        return;
-    }
     public static void main(String[] args) {
 
         PROVA p=new PROVA();
         p.setContentPane(p.getPanelMain());
         p.setVisible(true);
         p.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
     }
+
 }
--- src/Model/MainModel.java
+++ src/Model/MainModel.java
@@ -1,8 +1,20 @@
 package Model;
 
 public class MainModel {
-    public static void main(String[] args) {
-        System.out.println("CIAO" + "12345");
+    private int nRigheColonne=5;
+    private char lettere[]= {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+    private char Board[][] = new char[nRigheColonne][nRigheColonne];
 
+    public void creaBoard()
+    {
+        for(int i=0; i<nRigheColonne; i++)
+        {
+            for(int j=0; j<nRigheColonne; j++)
+            {
+                int nCasuale= (int) (Math.random()*lettere.length);
+                Board[i][j]=lettere[nCasuale];
+            }
+        }
+        return;
     }
 }
