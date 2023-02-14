c4da4f868bbfc0b7b5e8b1717a33fb6d4a7dfec9
PELANGELO <riccardo.angelo@edu.cobianchi.it>
2023-02-14 11:19:46 +0100

    COMMprova

---
+++ src/View/PROVA.java
@@ -0,0 +1,16 @@
+package View;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ComponentAdapter;
+
+public class PROVA extends JFrame{
+    private JRadioButton RadioButton;
+    private JPanel PanelMain;
+
+    public PROVA() {
+
+
+
+    }
+}
