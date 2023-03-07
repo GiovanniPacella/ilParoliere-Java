de909b28263658ea2b35e2c007ef153f1e638d61
PELANGELO <riccardo.angelo@edu.cobianchi.it>
2023-03-07 12:17:42 +0100

    COMMIT UI 07032023 3 ris size

--- src/Controller/MainController.java
+++ src/Controller/MainController.java
@@ -4,6 +4,7 @@ import View.PROVA;
 
 import javax.swing.*;
 
+import java.awt.*;
 import java.lang.Math;
 public class MainController {
     public static void main(String[] args) {
@@ -12,7 +13,11 @@ public class MainController {
         PROVA p=new PROVA();
         p.setContentPane(p.getPanelMain());
         p.setVisible(true);
+        p.getPanelMain().setSize(800,600);
         p.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        p.setSize(800,600);
+        /*Insets m = new Insets(20,20,20,20);
+        p.getButton2().setMargin(  m);*/
 
     }
 
